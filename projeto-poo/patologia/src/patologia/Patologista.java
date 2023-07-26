d78548d6688f72703406ff7457cd5df65f0c746c
Gustavo Ferreira Wanderley <mestreguga24@gmail.com>
2023-07-26 11:47:32 -0300

    Criação das classes "Pessoas"
    Foram criadas as classes: (Abstrata) Pessoa, Tutor, Veterinário e Patologista. Todas com suas heranças

---
+++ projeto-poo/patologia/src/patologia/Patologista.java
@@ -0,0 +1,9 @@
+package patologia;
+
+public class Patologista extends Veterinario {
+
+	public Patologista(String nome, String email, String telefone, String crmv, String instituicao) {
+		super(nome, email, telefone, crmv, instituicao);
+	}
+
+}
